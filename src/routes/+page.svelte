fb098eedff10d92cbffdb379231c61d5921909b5
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-12-23 22:10:13 +0000

    Add SvelteKit project with Bun

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let counter = 0
+</script>
+
+I'm counting ... {counter}
+<button
+	on:click={() => {
+		counter++
+	}}>Count up</button
+>
+
+<style lang="sass">
+    button
+        background: green
+        color:#FFF
+</style>
