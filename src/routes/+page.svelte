f09e69cdfad8bb3ee7803d276a9cd1d3c1dddfd2
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-12-23 23:58:35 +0000

    Fix merge conflicts?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,2 @@
-<<<<<<< HEAD
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-=======
-<script>
-	let counter = 0
-</script>
-
-I'm counting ... {counter}
-<button
-	on:click={() => {
-		counter++
-	}}>Count up</button
->
-
-<style lang="sass">
-    button
-        background: green
-        color:#FFF
-</style>
->>>>>>> fb098eedff10d92cbffdb379231c61d5921909b5
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -10,21 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter()
-=======
-import adapter from '@bun-community/sveltekit-adapter-bun';
-import preprocess from 'svelte-preprocess';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter({
-							 dynamic_origin: true
-						 })
->>>>>>> fb098eedff10d92cbffdb379231c61d5921909b5
 	}
 };
 
