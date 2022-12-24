1b38d972a555030d7402f344cb3d308b75d075cc
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-12-24 20:15:41 +0000

    Add markdown and attempt to add KaTeX

--- src/routes/[level]/[subject]/+page.svelte
+++ src/routes/[level]/[subject]/+page.svelte
@@ -1,24 +1,38 @@
 <script lang="ts">
+	import Katex from "svelte-katex"
+	import SvelteMarkdown from "svelte-markdown"
 	export let data: any
-	console.log(data)
+
+	function katexify(str: string) {
+		if (!str) return ""
+
+		if (str.includes("$$")) return str.replace(/\$\$(.*?)\$\$/g, (_, p1) => {
+			return `<Katex displayMode>${p1}</Katex>`
+		})
+
+		return str.replace(/\$(.*?)\$/g, (_, p1) => {
+			return `<Katex>${p1}</Katex>`
+		})
+
+
+	}
 </script>
 
 <svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ" crossorigin="anonymous" />
 	<title>Question • sqappq</title>
 </svelte:head>
 
 <h1>Question</h1>
-
 {data.level} {data.subject}
 <br />
 <br />
-{data.pre || ""}
+{katexify(data.pre)}
 <br />
 
 {#each data.c as question}
-	{question.q}
-	<br />
-	{question.post || ""}
+	<SvelteMarkdown source={katexify(question.q)} />
+	<SvelteMarkdown source={katexify(question.post)} />
 {/each}
 
 <style lang="sass">
