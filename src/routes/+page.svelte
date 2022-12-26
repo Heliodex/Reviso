a4a0b280fac620a8cbfb3331177d904802a1f919
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-12-26 04:46:51 +0000

    Add info about site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,18 @@
 	<title>Home • Reviso</title>
 </svelte:head>
 
+<h1>About</h1>
+
+<p>
+	Reviso is a site that provides random SQA past paper questions for revision.
+	It also provides marking schemes and answers for questions.<br />
+	<br />
+	At the moment, Reviso only supports a few questions from N5 Maths and Chemistry, but more will be added soon.<br />
+	<br />
+	Pick a level and subject and a question can be shown, reload the page to get a different question.
+	If you want to keep the question or send the link to someone, you can use the "Question link" button at the bottom of the page.
+</p>
+
 <h1>Levels</h1>
 
 <div id="grid">
@@ -9,6 +21,9 @@
 </div>
 
 <style lang="sass">
+	p
+		text-align: justify
+		max-width: 50rem
 	#grid
 		display: grid
 		grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr))
